d53b4aaee78efbf4f7fb861d8e08a274bc4196b1
Дарья Шипилова <sh.dari@list.ru>
2022-11-29 19:15:55 +0300

    task-react-render-and-effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+    const callback = (id: number) => {
+        setMessage(id);
+    };
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            setMessage(-1);
+            unsubscribe(props.sourceId, callback);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
